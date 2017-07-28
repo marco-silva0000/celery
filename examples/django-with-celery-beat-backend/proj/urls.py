4c99c06073e862b6d0b91b7b8d48c76587acc75a
marco silva <msilva@hubel.pt>
2017-07-28 22:41:21 +0100

    added demo project with celery beat as scheduler replicated timezone bug making celery beat lunch infinite tasks

---
+++ examples/django-with-celery-beat-backend/proj/urls.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, unicode_literals
+
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = []
